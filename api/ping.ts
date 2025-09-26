74aee56d278feadb23cb935aeffc273820a8ff56
Dai Inaoka <inaoka@makotoya.tech>
2025-09-26 11:14:47 +0900

    ping.ts追加

---
+++ api/ping.ts
@@ -0,0 +1,5 @@
+// api/ping.ts
+import type { VercelRequest, VercelResponse } from '@vercel/node';
+export default function handler(_req: VercelRequest, res: VercelResponse) {
+  res.status(200).send('pong');
+}
